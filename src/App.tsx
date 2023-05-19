713a2d26c601c4e68649fcaed0c0af78a135acda
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-05-19 15:48:07 -0400

    Use focusZone and add a forceRender function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 import { FluentPage } from "./FluentPage";
+import { ThemeProvider } from "@fluentui/react";
 
 export const App = () => {
   const [showFluentPage, setShowFluentPage] = React.useState(true);
+  const forceRender = useForceRender();
   return (
-    <div className="container">
-      <h1 className="display-2">Fluent Memory Leak</h1>
-      <p className="lead">
-        This page contains a reproduction of a memory leak in Fluent UI.
-      </p>
-      <p>
-        <button
-          className="btn btn-primary"
-          onClick={() => setShowFluentPage((prev) => !prev)}
-        >
-          {showFluentPage ? "Hide" : "Show"} page with FluentUI
-        </button>
-      </p>
-      {showFluentPage && (
-        <div className="p-3 border border-secondary-subtle rounded-3">
-          <FluentPage />
+    <>
+      <ThemeProvider>
+        <div className="container">
+          <h1 className="display-2">Fluent Memory Leak</h1>
+          <p className="lead">
+            This page contains a reproduction of a memory leak in Fluent UI.
+          </p>
+          <p>
+            <button
+              className="btn btn-primary"
+              onClick={() => setShowFluentPage((prev) => !prev)}
+            >
+              {showFluentPage ? "Hide" : "Show"} page with FluentUI
+            </button>
+            <button onClick={forceRender} className="btn btn-secondary">
+              Force Render
+            </button>
+          </p>
+          {showFluentPage && (
+            <div className="p-3 border border-secondary-subtle rounded-3">
+              <FluentPage />
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </ThemeProvider>
+    </>
   );
 };
+
+function useForceRender() {
+  const [, updateState] = React.useState<any>();
+  return React.useCallback(() => updateState({}), []);
+}
--- src/FluentPage.tsx
+++ src/FluentPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { ThemeProvider, DefaultButton } from "@fluentui/react";
+import { ThemeProvider, FocusZone, DefaultButton, Link } from "@fluentui/react";
 
 export const FluentPage = () => (
   <ThemeProvider>
-    <DefaultButton onClick={() => alert("hello")}>
-      Fluent DefaultButton Component
-    </DefaultButton>
+    <iframe style={{ border: "1px solid red" }}></iframe>
+    <FocusZone>
+      <DefaultButton onClick={() => alert("hello")}>
+        Fluent DefaultButton Component FocusZone
+      </DefaultButton>
+      <Link href="https://bing.com" target="_blank">
+        Link component FocusZone
+      </Link>
+    </FocusZone>
   </ThemeProvider>
 );
